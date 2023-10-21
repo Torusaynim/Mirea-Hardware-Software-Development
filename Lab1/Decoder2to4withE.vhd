02646d3d26ab546e5b9e27c5d1a3dcb6d408af90
Torusaynim <56824839+Torusaynim@users.noreply.github.com>
2023-10-21 14:33:07 +0300

    Fix tabs

--- Lab1/Decoder2to4withE.vhd
+++ Lab1/Decoder2to4withE.vhd
@@ -3,20 +3,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity Decoder2to4withE is
-    Port ( a1 : in  STD_LOGIC;
-           a0 : in  STD_LOGIC;
-           e : in  STD_LOGIC;
-           q : out  STD_LOGIC_VECTOR (3 downto 0));
+  Port ( a1 : in  STD_LOGIC;
+         a0 : in  STD_LOGIC;
+         e : in  STD_LOGIC;
+         q : out  STD_LOGIC_VECTOR (3 downto 0));
 end Decoder2to4withE;
 
 architecture Behavioral of Decoder2to4withE is
 
 begin
-	q(0) <= e and not a1 and not a0;
-	q(1) <= e and not a1 and a0;
-	q(2) <= e and a1 and not a0;
-	q(3) <= e and a1 and a0;
-
+  
+  q(0) <= e and not a1 and not a0;
+  q(1) <= e and not a1 and a0;
+  q(2) <= e and a1 and not a0;
+  q(3) <= e and a1 and a0;
 
 end Behavioral;
-
--- Lab1/Test4to16.vhd
+++ Lab1/Test4to16.vhd
@@ -9,51 +9,52 @@ END Test4to16;
 ARCHITECTURE behavior OF Test4to16 IS 
  
  
-    COMPONENT Decoder4to16withE
-    PORT(
-         a : IN  std_logic_vector(3 downto 0);
-         e : IN  std_logic;
-         q : OUT  std_logic_vector(15 downto 0)
-        );
-    END COMPONENT;
+  COMPONENT Decoder4to16withE
+  PORT(
+       a : IN  std_logic_vector(3 downto 0);
+       e : IN  std_logic;
+       q : OUT  std_logic_vector(15 downto 0)
+      );
+  END COMPONENT;
     
 
-   --Inputs
-   signal a : std_logic_vector(3 downto 0) := (others => '0');
-   signal e : std_logic := '0';
+  --Inputs
+  signal a : std_logic_vector(3 downto 0) := (others => '0');
+  signal e : std_logic := '0';
 
- 	--Outputs
-   signal q : std_logic_vector(15 downto 0); 
+  --Outputs
+  signal q : std_logic_vector(15 downto 0); 
  
  
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
-   uut: Decoder4to16withE PORT MAP (
-          a => a,
-          e => e,
-          q => q
-        );
+  -- Instantiate the Unit Under Test (UUT)
+  uut: Decoder4to16withE PORT MAP (
+        a => a,
+        e => e,
+        q => q
+      );
  
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 10 ns.
-      -- wait for 10 ns;	
-		e <= '0';
-		for i in 0 to 15 loop
-			a <= std_logic_vector(to_unsigned(i,4));
-			wait for 5 ns;
-		end loop;
-		
-		e <= '1';
-		for i in 0 to 15 loop
-			a <= std_logic_vector(to_unsigned(i,4));
-			wait for 5 ns;
-		end loop;
-		
-      wait;
-   end process;
+  -- Stimulus process
+  stim_proc: process
+  begin    
+    -- hold reset state for 10 ns.
+    -- wait for 10 ns;
+    
+    e <= '0';
+    for i in 0 to 15 loop
+      a <= std_logic_vector(to_unsigned(i,4));
+      wait for 5 ns;
+    end loop;
+    
+    e <= '1';
+    for i in 0 to 15 loop
+      a <= std_logic_vector(to_unsigned(i,4));
+      wait for 5 ns;
+    end loop;
+    
+    wait;
+  end process;
 
 END;
